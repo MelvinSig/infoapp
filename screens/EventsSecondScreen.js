2de536c07dde0077f8e70ab82bf391cc946c92e4
MelvinSig <melvinsonywm@gmail.com>
2025-08-30 19:03:32 +0800

    Remove legacy screens: Contact/Events/EventsSecond

--- screens/EventsSecondScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-const EventsSecondScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Events Second Screen Details!</Text>
-  </View>
-);
-
-export default EventsSecondScreen;
